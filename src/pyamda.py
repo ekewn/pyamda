cc834db81810fead3a371f9e79558f95c0faae72
Ellis Coon <ellis.goon@gmail.com>
2024-11-23 13:59:13 -0700

    wip - modularizing

--- src/pyamda.py
+++ src/pyamda.py
@@ -672,80 +672,76 @@ def endswith[a](val: a, l: List[a]) -> bool:
 
 type NewDict = Dict
 
-def get[a, b](d: Dict[a, b], default: b, key: a) -> b:
+class D:
     """
-    Dict.get alias.
+    Bundle of dictionary related functions.
+    All functions are staticmethods.
     """
-    return d.get(key, default)
+
+    @staticmethod
+    def get[a, b](key: a) -> FnU[Dict[a,b], Optional[b]]:
+        """
+        Curried dict.get alias.
+        """
+        return op.methodcaller("get", key)
 
 
-def prop(key: str, d: Dict[str, Any]) -> Optional[Any]:
-    """
-    Recursively checks the object to return the value at the key, if present, else None.
-    """
-    if key in d:
-        return d[key]
-    else:
-        for v in list(filter(is_dict, d.values())):
-            return prop(key, v)
+    @staticmethod
+    def prop(key: str) -> FnU[Dict[str, Any], Optional[Any]]:
+        """
+        Returns a function that recursively checks the object to return the value at the key, if present, else None.
+        """
+        def _(key, d) -> Optional[Any]:
+            if key in d:
+                return d[key]
+            else:
+                for v in list(filter(is_dict, d.values())):
+                    return D.prop(key)(v) #type: ignore
+        return p(_, key)
 
 
-def props(keys: List[str], d: Dict[str, Any]) -> List[Optional[Any]]:
-    """
-    Returns values from multiple keys if they exist on the object.
-    """
-    return list(map(partial(flip(prop), d), keys))
+    @staticmethod
+    def prop_or[a](key: str, default: a) -> FnU[Dict[str, a], a]:
+        """
+        Returns a function that, given a dict, returns the default value if key is not found in the object.
+        """
+        return default_with(default, D.prop(key))
 
 
-def prop_or(key: str, default: Any, d: Dict[str, Any]) -> Any:
-    """
-    Returns default value if key is not found in the object.
-    """
-    return default if prop(key, d) is None else prop(key, d)
+    @staticmethod
+    def pick(keys: List[str]) -> FnU[Dict[str, Any], Dict[str, Any]]:
+        """
+        Returns partial copies of the given dictionary with only the specified keys.
+        Any keys that don't exist in the dictionary will not be included in the copy,
+        i.e. they'll raise a KeyError if you try to attempt them.
+        """
+        def _(keys, d): return {k: v for k, v in d.items() if k in keys}
+        return p(_, keys)
 
 
-def prop_eq(key: str, val: Any, d: Dict[str, Any]) -> bool:
-    """
-    Returns true if key is in the object.
-    """
-    return prop(key, d) == val
+    @staticmethod
+    def omit(keys: List[str]) -> FnU[Dict[str, Any], Dict[str, Any]]:
+        """
+        Returns partial copies of the given dictionary with the specified keys dropped.
+        """
+        def _ (keys, d): return {k: v for k, v in d.items() if k not in keys}
+        return p(_, keys)
 
 
-def prop_satisfies(key: str, p: Predicate[Any], d: Dict[str, Any]) -> bool:
-    """
-    Returns true if prop satisfies the predicate.
-    """
-    return p(prop(key, d))
+    @staticmethod
+    def project(keys: List[str], ds: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        """
+        Think of this as an SQL select query. Gets the props off of each dict in the list.
+        """
+        return list(map(D.pick(keys), ds))
 
 
-def pick(keys: List[str], d: Dict[str, Any]) -> Dict[str, Any]:
-    """
-    Returns partial copies of the given dictionary with only the specified keys.
-    Any keys that don't exist in the dictionary will not be included in the copy,
-    i.e. they'll raise a KeyError if you try to attempt them.
-    """
-    return {k: v for k, v in d.items() if k in keys}
-
-
-def omit(keys: List[str], d: Dict[str, Any]) -> Dict[str, Any]:
-    """
-    Returns partial copies of the given dictionary with the specified keys dropped.
-    """
-    return {k: v for k, v in d.items() if k not in keys}
-
-
-def project(keys: List[str], ds: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
-    """
-    Think of this as an SQL select query. Gets the props off of each dict in the list.
-    """
-    return list(map(partial(pick, keys), ds))
-
-
-def apply_spec[a, b](fn: FnU, spec: Dict[str, a]) -> Dict[str, b]: #type: ignore - b only shows up once in signature
-    """
-    Applies the provided function to each value in the provided dictionary.
-    """
-    return {k: fn(v) for k, v in spec}
+    @staticmethod
+    def apply_spec[a, b](fn: FnU, d: Dict[str, a]) -> Dict[str, b]: #type: ignore - b only shows up once in signature
+        """
+        Applies the provided function to each value in the provided dictionary.
+        """
+        return {k: fn(v) for k, v in d}
 
 
 # Class Instance Functions
@@ -788,22 +784,53 @@ def attr_eq(name: str, val: Any) -> FnU[object, bool]:
 
 # String Functions
 
-type NewStr = str
+class S:
+    @staticmethod
+    def replace(old: str, new: str) -> FnU[str, str]:
+        """
+        Curried, pure s.replace().
+        """
+        return method("replace", old, new)
 
-def replace(old: str, new: str, s: str) -> NewStr:
-    """
-    Pure s.replace()
-    """
-    s2 = s
-    return s2.replace(old, new)
+
+    @staticmethod
+    def split(sep: str, maxsplits: int = -1) -> FnU[str, List[str]]:
+        """
+        Curried, pure s.split().
+        """
+        return method("split", sep, maxsplits)
 
 
-def split(sep: str, s: str, maxsplits: int = -1) -> List[NewStr]:
-    """
-    Pure s.split().
-    """
-    s2 = s
-    return s2.split(sep, maxsplits)
+    @staticmethod
+    def join_with(sep: str) -> FnU[List[str], str]:
+        """
+        Curried, pure s.join().
+        """
+        return sep.join
+
+
+    @staticmethod
+    def title() -> FnU[str, str]:
+        """
+        Curried, pure s.title().
+        """
+        return method("title")
+
+
+    @staticmethod
+    def lower() -> FnU[str, str]:
+        """
+        Curried, pure s.lower().
+        """
+        return method("lower")
+
+
+    @staticmethod
+    def upper() -> FnU[str, str]:
+        """
+        Curried, pure s.upper().
+        """
+        return method("upper")
 
 
 # Mathematical Functions
