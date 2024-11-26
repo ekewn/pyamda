fba9fe528e440e207e369206502dfd95df8a3c5a
Ellis Coon <ellis.coon@equusoft.com>
2024-11-26 15:45:33 -0700

    just need to write tests for lists now

--- src/classes.py
+++ src/classes.py
@@ -6,6 +6,13 @@ from core import op, partial, FnU
 def attr(name: str) -> FnU[object, Optional[Any]]:
     """
     Returns a function that given an object returns the value of the object attribute if present, or none.
+
+    >>> class Test:
+    ...     one = 1
+    >>> get_one = attr("one")
+    >>> get_two = attr("two")
+    >>> assert get_one(Test()) == 1
+    >>> assert get_two(Test()) == None
     """
 
     def _(n, obj):
@@ -20,6 +27,13 @@ def attr(name: str) -> FnU[object, Optional[Any]]:
 def attr_or(name: str, default: Any) -> FnU[object, Any]:
     """
     Returns a function that given an object returns the value of the object attribute if present, or none.
+
+    >>> class Test:
+    ...     one = 1
+    >>> get_one_or_0 = attr_or("one", 0)
+    >>> get_two_or_12 = attr_or("two", 12)
+    >>> assert get_one_or_0(Test()) == 1
+    >>> assert get_two_or_12(Test()) == 12
     """
 
     def _(n, obj):
@@ -34,6 +48,13 @@ def attr_or(name: str, default: Any) -> FnU[object, Any]:
 def attr_eq(name: str, val: Any) -> FnU[object, bool]:
     """
     Returns a function that given an object returns if the value of the object attribute is equal to the val.
+
+    >>> class Test:
+    ...     one = 1
+    >>> one_is_1 = attr_eq("one", 1)
+    >>> one_is_2 = attr_eq("two", 2)
+    >>> assert one_is_1(Test())
+    >>> assert not one_is_2(Test())
     """
 
     def _(n, v, o):
