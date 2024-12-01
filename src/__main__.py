a9279732157e4fed5a55919856f5a6ef92cfe066
Ellis Coon <ellis.coon@equusoft.com>
2024-12-01 13:12:04 -0700

    Updated __main__ test script

--- src/__main__.py
+++ src/__main__.py
@@ -1,35 +1,39 @@
 import os
 import doctest
-import importlib.util
+import importlib.util as U
 import sys
+from typing import Optional
 
 
-def run_doctests_in_directory(directory):
-    """
-    Runs doctests for all .py files in the given directory, excluding __main__.py.
-    """
-    for filename in os.listdir(directory):
-        if filename.endswith(".py") and filename != "__main__.py":
-            module_name = filename[:-3]  # Strip the .py extension
-            file_path = os.path.join(directory, filename)
+def run_doctests(directory: str, filename: str) -> Optional[doctest.TestResults]:
+    module_name = filename[:-3]  # Strip the .py extension
+    file_path = os.path.join(directory, filename)
 
-            try:
-                # Dynamically import the module
-                spec = importlib.util.spec_from_file_location(module_name, file_path)
-                assert spec is not None
-                module = importlib.util.module_from_spec(spec)
-                sys.modules[module_name] = module
-                loader = spec.loader
-                assert loader is not None
-                loader.exec_module(module)
+    spec = U.spec_from_file_location(module_name, file_path)
+    assert spec is not None, f"{module_name}, {file_path}"
 
-                # Run doctests on the module
-                print(f"Running doctests in {filename}...")
-                doctest.testmod(module, verbose=True)
-            except Exception as e:
-                print(f"Failed to run doctests in {filename}: {e}")
+    module = U.module_from_spec(spec)
+    sys.modules[module_name] = module
+
+    loader = spec.loader
+    assert loader is not None
+    loader.exec_module(module)
+
+    return doctest.testmod(module)
 
 
 if __name__ == "__main__":
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    run_doctests_in_directory(current_directory)
+    print("Running Module Tests\n ------------------------------")
+    directory = os.path.dirname(os.path.abspath(__file__))
+    filenames = [
+        f
+        for f in os.listdir(directory)
+        if f.endswith(".py") and f not in {"__main__.py", "__init__.py"}
+    ]
+    for filename in filenames:
+        results = run_doctests(directory, filename)
+        (
+            print(f"All tests for {filename} passed!")
+            if results is not None and results.failed == 0
+            else print()
+        )
