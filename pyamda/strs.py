38a75e05cb693ab4afb4e1ccb0245079394cd76d
Ellis Coon <ellis.coon@equusoft.com>
2024-12-05 15:58:38 -0700

    2.7.0 - added strs.concat

--- pyamda/strs.py
+++ pyamda/strs.py
@@ -1,7 +1,7 @@
 from typing import List
 from operator import methodcaller as method
 
-from pyamda.core import FnU
+from pyamda.core import FnU, partial
 
 
 def replace(old: str, new: str) -> FnU[str, str]:
@@ -13,14 +13,38 @@ def replace(old: str, new: str) -> FnU[str, str]:
     return method("replace", old, new)
 
 
-def remove(s: str) -> FnU[str, str]:
+def remove(s: str | List[str]) -> FnU[str, str]:
     """
     Replaces the given string with "" in the string fed to the resulting function.
 
     >>> assert remove("a")("abc") == "bc"
     >>> assert remove("b")("abc") == "ac"
+    >>> assert remove(["a", "b"])("abc") == "c"
+    >>> assert remove(["x", "y"])("xyz") == "z"
     """
-    return method("replace", s, "")
+    if isinstance(s, str):
+
+        def remover(input_str: str) -> str:
+            return input_str.replace(s, "")
+
+    else:
+
+        def remover(input_str: str) -> str:
+            for substring in s:
+                input_str = input_str.replace(substring, "")
+            return input_str
+
+    return remover
+
+
+def concat(s: str) -> FnU[str, str]:
+    """
+    Returns a function that adds this string to the end of the argument to the resulting function.
+
+    >>> assert concat("new")("old") == "oldnew"
+    >>> assert concat("last")("first") == "firstlast"
+    """
+    return method("__add__", s)
 
 
 def split(sep: str, maxsplits: int = -1) -> FnU[str, List[str]]:
